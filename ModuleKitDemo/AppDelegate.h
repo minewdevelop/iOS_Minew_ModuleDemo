b0163c1cb6eeb08927c74f73d8c2de3a9a3df197
rex.xie <rex.xie@minewtech.com>
2020-11-25 09:36:58 +0800

    Initial Commit

---
+++ ModuleKitDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ModuleKitDemo
+//
+//  Created by Minewtech on 2020/11/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ModuleKitDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ModuleKitDemo
+//
+//  Created by Minewtech on 2020/11/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
